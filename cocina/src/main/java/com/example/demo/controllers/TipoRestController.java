fc5afc4a07a39e7064b8dd88830c75fd00c2cf4f
arkeyn2 <103384439+arkeyn2@users.noreply.github.com>
2024-01-26 15:00:20 -0300

    tabla tipo y complemetos

---
+++ cocina/src/main/java/com/example/demo/controllers/TipoRestController.java
@@ -0,0 +1,121 @@
+package com.example.demo.controllers;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.models.entity.Tipo;
+import com.example.demo.models.services.ITipoService;
+
+@CrossOrigin(origins ={"http://loalhost:4200","*"})
+@RestController
+@RequestMapping("/api")
+public class TipoRestController {
+
+	@Autowired
+	private ITipoService tipoService;
+	
+	@GetMapping("/tipos")
+	public List<Tipo>index(){
+		return tipoService.findAll();
+	}
+	
+	@GetMapping("/tipos{id}")
+	public ResponseEntity<?> show(@PathVariable Long id){
+		
+		Tipo tipos = null;
+		Map<String, Object> response = new HashMap<>();
+		
+		try {
+			tipos = tipoService.findById(id);
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al realizar la consulta a la base de datos");
+			response.put("error",e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		
+		if (tipos == null) {
+			response.put("mensaje", "El tipo id:".concat(id.toString().concat(" no existe en la base de datos!")));
+		}
+		return new ResponseEntity<Tipo>(tipos, HttpStatus.OK);
+	}
+	
+	@PostMapping("/tipos")
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<?> create (@RequestBody Tipo tipos){
+	
+		Tipo tiposnew =null;
+		Map<String, Object> response = new HashMap<>();
+		
+		try {
+			tiposnew = tipoService.save(tipos);
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al realizar el insert en la base de datos");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		response.put("mensaje", "El tipos ha sido creado con exito!");
+		response.put("tipos", tiposnew);
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+	}
+	
+	@PostMapping("/tipos/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<?> update(@RequestBody Tipo tipos, @PathVariable Long id ){
+	
+		Tipo tiposActual =tipoService.findById(id);
+		
+		Tipo tiposUpdate =null;
+		
+		Map<String, Object> response = new HashMap<>();
+		
+		if (tipos == null) {
+			response.put("mensaje", "El tipo Id:" .concat(id.toString().concat(" no existe en la base de datos!")));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		
+		}
+		try {
+			tiposActual.setNombre(tipos.getNombre());
+
+			
+			tiposUpdate = tipoService.save(tiposActual);
+		}catch (DataAccessException e) {
+			response.put("mensaje", "Error al realizar actualizado en la base de datos");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+	
+		}
+		response.put("mensaje", "El tipos ha sido actualizado con exito!");
+		response.put("tipos", tiposUpdate);
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping("/tipos/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		Map<String, Object> response = new HashMap<>();
+		try {
+			tipoService.delete(id);
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al eliminar en la base de datos");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		response.put("mensaje", "El tipo fue eliminada con exito!");
+
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
+	}
+}
---
+++ cocina/src/main/java/com/example/demo/models/entity/Tipo.java
@@ -0,0 +1,45 @@
+package com.example.demo.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tipo")
+public class Tipo implements Serializable{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable = false)
+	private String nombre;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
